b25d765482555ff73b448df933126b8462467c2e
raghu <raghukjnv@gmail.com>
2019-08-22 21:20:17 +0530

    adding files

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {   
+
+         int a,b;
+         a=90;
+         b=100;
+        System.out.println( "Sum of the values" );
+       
+       System.out.println( a+b );
+
+    }
+}
